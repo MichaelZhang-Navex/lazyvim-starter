273cc1f730cd4726c2a874f5693b1dd2243f9c6e
Michael Zhang <michael.zhang@navex.com>
2025-10-30 15:31:28 -0500

    my personal config

---
+++ lua/plugins/markdown-lint.lua
@@ -0,0 +1,12 @@
+local CONFIG_HOME = '/home/michael/.config/nvim/'
+return {
+  "mfussenegger/nvim-lint",
+  optional = true,
+  opts = {
+    linters = {
+      ["markdownlint-cli2"] = {
+        args = { "--config", CONFIG_HOME .. "lua/plugins/markdownlint-cli2.yaml", "--" },
+      },
+    },
+  },
+}
